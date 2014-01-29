c2ec6ef03494ccd19dddd2fd4017fb4aab3eaa7c
Hiroto Takahashi <elzzup.htam@i.softbank.jp>
2014-01-29 10:36:08 +0900

    add names

--- src/Human.java
+++ src/Human.java
@@ -1,4 +1,30 @@
 
 public class Human {
+	private String firstName;
+	private String lastName;
+	private String middleName;
 
+	private int sex;
+	public static final int MALE = 0;
+	public static final int FEMALE = 1;
+
+	public String getFirstName () {
+		return this.firstName;
+	}
+	public String getLastName () {
+		return this.lastName;
+	}
+	public String getMiddleName () {
+		return this.middleName;
+	}
+	public boolean hasMiddleName () {
+		return this.middleName != null;
+	}
+	public static final String NAME_SEPARATER = " ";
+	public String getFullName () {
+		return this.firstName + NAME_SEPARATER + (this.hasMiddleName() ? this.middleName:"") + this.lastName;
+	}
+
+	public Human(String firstName, String lastName) {
+	}
 }
